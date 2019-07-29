7f3451b3cd02c2cc931a5c3d3ebf6eb6fd010af1
Pratap A K <pratap.ak@ge.com>
2019-07-29 17:50:57 +0530

    Revert "aborting build if quality gate is not ok"
    This reverts commit 8d7920e3

--- vars/deliveryPipelineParent.groovy
+++ vars/deliveryPipelineParent.groovy
@@ -59,15 +59,6 @@ def call(body) {
                     }
                 }
             }
-
-            stage("Quality Gate"){
-                timeout(time: 1, unit: 'HOURS') {
-                    def qg = waitForQualityGate()
-                    if (qg.status != 'OK') {
-                        error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                    }
-                }
-            }
         }
 
         post {
