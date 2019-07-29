8d7920e3e3286e1f2b48fc48cfc80a60d35a3cf4
Pratap A K <pratap.ak@ge.com>
2019-07-29 17:36:15 +0530

    aborting build if quality gate is not ok

--- vars/deliveryPipelineParent.groovy
+++ vars/deliveryPipelineParent.groovy
@@ -59,6 +59,15 @@ def call(body) {
                     }
                 }
             }
+
+            stage("Quality Gate"){
+                timeout(time: 1, unit: 'HOURS') {
+                    def qg = waitForQualityGate()
+                    if (qg.status != 'OK') {
+                        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                    }
+                }
+            }
         }
 
         post {
