c4ccd48874fa8bdf9ef03f396633493079bf0131
Pratap A K <pratap.ak@ge.com>
2019-07-30 14:02:27 +0530

    ssh command

--- vars/deliveryPipelineParent.groovy
+++ vars/deliveryPipelineParent.groovy
@@ -4,6 +4,14 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
     body()
+
+    def remote = [:]
+    remote.name = 'VM Server'
+    remote.host = '10.184.96.145'
+    remote.user = 'digital'
+    remote.password = 'Password@123'
+    remote.allowAnyHosts = true
+
     pipeline {
         agent any
 
@@ -71,7 +79,10 @@ def call(body) {
                 }
             }
 
-
+            stage('Remote SSH') {
+                sshCommand remote: remote, command: "ls -lrt"
+                sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
+            }
         }
 
         post {
