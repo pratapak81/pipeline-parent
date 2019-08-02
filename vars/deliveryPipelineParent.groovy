300a40ce66956e51fda10843378f55de59ffda4e
Pratap A K <pratap.ak@ge.com>
2019-08-02 11:07:35 +0530

    docker tool

--- vars/deliveryPipelineParent.groovy
+++ vars/deliveryPipelineParent.groovy
@@ -96,6 +96,19 @@ def call(body) {
                 }
             }
 
+            stage {
+                agent {
+                    docker { image 'node:7-alpine' }
+                }
+                stages {
+                    stage('Test') {
+                        steps {
+                            sh 'node --version'
+                        }
+                    }
+                }
+            }
+
             /*stage('Docker') {
                 agent {
                     dockerfile true
